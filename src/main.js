b9666ba720e4e09a23444796b7da0eadda929fbe
Alan Kydd <akydd@ualberta.net>
2015-06-30 23:38:14 -0600

    removed unused file

--- src/main.js
+++
@@ -1,7 +0,0 @@
-var game = new Phaser.Game(520, 600, Phaser.AUTO, 'game-div');
-
-game.state.add('Load', Arkanoid.Load);
-game.state.add('Menu', Arkanoid.Menu);
-game.state.add('Game', Arkanoid.Game);
-
-game.state.start('Load');
