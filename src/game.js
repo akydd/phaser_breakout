a17bfb9d027cb71abd3e91a568af57924705f5c0
Alan Kydd <akydd@ualberta.net>
2015-07-02 22:58:30 -0600

    Fixed up game loop to work again.

--- src/game.js
+++ src/game.js
@@ -55,6 +55,7 @@ define(['phaser', 'phaser-game'], function(Phaser, game) {
 
             // the ball (8x8)
             this.ball = game.add.sprite(game.world.centerX - 4, this.paddle.body.y - 8, 'ball');
+            this.ballOnPaddle = true;
             game.physics.enable(this.ball);
             // the ball should bounce off the edges of the world
             this.ball.body.collideWorldBounds = true;
@@ -62,19 +63,20 @@ define(['phaser', 'phaser-game'], function(Phaser, game) {
             this.ball.body.bounce.y = 1;
             // handle lost balls 
             this.ball.checkWorldBounds = true;
-            this.ball.events.onOutOfBounds.add(ballLost, this);
+            this.ball.events.onOutOfBounds.add(this.ballLost, this);
 
             // the score
+            this.score = 0;
             var style = { font: "20px Arial", fill: "#000000" };  
-            var scoreText = game.add.text(20, 20, "Score: 0", style);
+            this.scoreText = game.add.text(20, 20, "Score: " + this.score, style);
 
             // lives
-            var livesText = game.add.text(20, 40, "Lives: 3", style);
+            this.lives = 3;
+            this.livesText = game.add.text(20, 40, "Lives: " + this.lives, style);
 
             // get ready for keyboard input
             this.cursors = game.input.keyboard.createCursorKeys();
         },
-
         update: function() {
             // paddle motion
             this.paddle.body.velocity.x = 0;
@@ -85,7 +87,7 @@ define(['phaser', 'phaser-game'], function(Phaser, game) {
             }
 
             if (this.cursors.down.isDown && this.ballOnPaddle) {
-                releaseBall();
+                this.releaseBall();
             }
 
             if (this.ballOnPaddle) {
@@ -96,84 +98,76 @@ define(['phaser', 'phaser-game'], function(Phaser, game) {
             game.physics.arcade.collide(this.ball, this.paddle, bounceOffPaddle, null, this);
 
             // ball block collision
-            game.physics.arcade.collide(this.ball, this.blocks, hitBlock, null, this);
+            game.physics.arcade.collide(this.ball, this.blocks, this.hitBlock, null, this);
+        },
+        releaseBall: function() {
+            this.ballOnPaddle = false;
+            // release at pi/4, angular velocity of 250, plus horizontal velocity of
+            // paddle
+            this.ball.body.velocity.x = 177 + this.paddle.body.velocity.x;
+            this.ball.body.velocity.y = -177;
+        },
+        hitBlock: function(ball, block) {
+            block.damage(1);
+
+            switch(block.key) {
+                case 'block_white':
+                    this.score += 50;
+                    break;
+                case 'block_orange':
+                    this.score += 60;
+                    break;
+                case 'block_turquoise':
+                    this.score += 70;
+                    break;
+                case 'block_green':
+                    this.score += 80;
+
+                    // Testing powerup
+                    var powerup  = game.add.sprite(block.x, block.y, 'disrupt');
+                    game.physics.enable(powerup);
+                    powerup.body.velocity.y = 200;
+                    // handle lost balls 
+                    powerup.checkWorldBounds = true;
+                    // replace this with outOfBounds.kill = true;
+                    //powerup.events.onOutOfBounds.add(ballLost, this);
+
+                    break;
+                case 'block_red':
+                    this.score += 90;
+                    break;
+                case 'block_blue':
+                    this.score += 100;
+                    break;
+                case 'block_pink':
+                    this.score += 110;
+                    break;
+                case 'block_yellow':
+                    this.score += 120;
+                    break;
+                case 'block_silver':
+                    if (!block.alive) {
+                        this.score += 50;
+                    }
+            }
+
+            this.scoreText.text = "Score: " + this.score;
+        },
+        ballLost: function() {
+            this.lives--;
+            this.livesText.text = "Lives: " + this.lives;
+
+            this.ball.reset(this.paddle.body.x, this.paddle.body.y - 8);
+            this.ballOnPaddle = true;
+
+            if (this.lives == 0) {
+                game.state.start('Menu');
+            }
         }
     };
 
     return Game;
 });
-
-function releaseBall() {
-    this.ballOnPaddle = false;
-    // release at pi/4, angular velocity of 250, plus horizontal velocity of
-    // paddle
-    this.ball.body.velocity.x = 177 + this.paddle.body.velocity.x;
-    this.ball.body.velocity.y = -177;
-}
-
-function ballLost() {
-    lives--;
-    this.livesText.text = "Lives: " + lives;
-
-    this.ball.reset(this.paddle.body.x, this.paddle.body.y - 8);
-    this.ballOnPaddle = true;
-
-    if (lives == 0) {
-        score = 0;
-        lives = 3;
-        game.state.start('Menu');
-    }
-}
-
-function killPowerUp() {
-}
-
-function hitBlock(ball, block) {
-    block.damage(1);
-
-    switch(block.key) {
-        case 'block_white':
-            score += 50;
-            break;
-        case 'block_orange':
-            score += 60;
-            break;
-        case 'block_turquoise':
-            score += 70;
-            break;
-        case 'block_green':
-            score += 80;
-
-            // Testing powerup
-            var powerup  = this.game.add.sprite(block.x, block.y, 'disrupt');
-            game.physics.enable(powerup);
-            powerup.body.velocity.y = 200;
-            // handle lost balls 
-            powerup.checkWorldBounds = true;
-            // replace this with outOfBounds.kill = true;
-            //powerup.events.onOutOfBounds.add(ballLost, this);
-
-            break;
-        case 'block_red':
-            score += 90;
-            break;
-        case 'block_blue':
-            score += 100;
-            break;
-        case 'block_pink':
-            score += 110;
-            break;
-        case 'block_yellow':
-            score += 120;
-            break;
-        case 'block_silver':
-            if (!block.alive) {
-                score += 50;
-            }
-    }
-
-    this.scoreText.text = "Score: " + score;
-}
 
 function bounceOffPaddle() {
     // the physics are handled by the framework.
